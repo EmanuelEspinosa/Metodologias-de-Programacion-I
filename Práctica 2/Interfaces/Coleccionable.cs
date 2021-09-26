e0091215b5c74410cac3a1069b6ced2f7b909de8
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2021-09-26 03:31:35 -0300

    Práctica 2 - Se implementan las interfaces requeridas para esta práctica

---
+++ Práctica 2/Interfaces/Coleccionable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_2
+{
+    interface Coleccionable
+    {
+        int Cuantos();
+        Comparable minimo();
+        Comparable maximo();
+        void agregar(Comparable n);
+        bool contiene(Comparable n);
+    }
+}
