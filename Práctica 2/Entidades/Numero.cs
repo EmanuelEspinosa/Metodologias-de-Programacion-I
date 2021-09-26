da1c9a51d82f1be95c604b9cd1f834b703611410
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2021-09-26 20:38:11 -0300

    Práctica 2 - Se implementan las clases Diccionario, Numero, Conjunto, claveValor, Iterable <Interface> y DiccionarioIterable

---
+++ Práctica 2/Entidades/Numero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_2
+{
+    class Numero : Comparable
+    {
+        private int valor;
+        public Numero(int valor) { this.valor = valor; }
+        public int getValor() { return valor; }
+
+        public bool sosIgual(Comparable n)
+        {
+            return this.valor == ((Numero)n).valor;
+        }
+
+        public bool sosMayor(Comparable n)
+        {
+            return this.valor > ((Numero)n).getValor();
+        }
+
+        public bool sosMenor(Comparable n)
+        {
+            return this.valor < ((Numero)n).getValor();
+        }
+        public override string ToString()
+        {
+            return (valor).ToString();
+        }
+    }
+}
