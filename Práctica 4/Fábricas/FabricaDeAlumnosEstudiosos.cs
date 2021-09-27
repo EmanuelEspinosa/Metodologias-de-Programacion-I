e6e1df72a57891bc0bbd159ee8f1225ec1a78288
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2021-09-27 01:39:09 -0300

    Práctica 4 - Se implementan las estrategias de comparación y las fábricas

---
+++ Práctica 4/Fábricas/FabricaDeAlumnosEstudiosos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_4
+{
+    class FabricaDeAlumnosEstudiosos : fabricaDeComparables
+    {
+        GeneradorDeDatosAleatorios gen = new GeneradorDeDatosAleatorios();
+        LectorDeDatos lector = new LectorDeDatos();
+
+        public override Comparable crearAleatorio()
+        {
+
+            return new AlumnoMuyEstudioso(gen.stringAleatorio(15), gen.numeroAleatorio(1000), gen.numeroAleatorio(1000), gen.numeroAleatorio(10));
+        }
+
+        public override Comparable crearPorTeclado()
+        {
+            return new AlumnoMuyEstudioso(lector.stringPorTeclado(), lector.numeroPorTeclado(), lector.numeroPorTeclado(), lector.numeroPorTeclado());
+        }
+    }
+}
---
+++ Práctica 4/Fábricas/fabricaDeComparables.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_4
+{
+    abstract class fabricaDeComparables : FabricaDeComparables
+    {
+        private const int ALUMNOS = 1;
+        private const int NUMEROS = 2;
+        private const int VENDEDORES = 3;
+        private const int ALUMNOS_MUY_ESTUDIOSOS = 4;
+
+        public abstract Comparable crearAleatorio();
+        public abstract Comparable crearPorTeclado();
+
+        public static Comparable crearAleatorio(int opcion)
+        {
+            FabricaDeComparables fabrica = null;
+            switch (opcion)
+            {
+                case ALUMNOS:
+                    fabrica = new FabricaDeAlumnos();
+                    break;
+                case NUMEROS:
+                    fabrica = new FabricaDeNumeros();
+                    break;
+                case VENDEDORES:
+                    fabrica = new FabricaDeVendedores();
+                    break;
+                case ALUMNOS_MUY_ESTUDIOSOS:
+                    fabrica = new FabricaDeAlumnosEstudiosos();
+                    break;
+                default:
+                    break;
+            }
+            return fabrica.crearAleatorio();
+        }
+        public static Comparable crearPorTeclado(int opcion)
+        {
+            FabricaDeComparables fabrica = null;
+            switch (opcion)
+            {
+                case ALUMNOS:
+                    fabrica = new FabricaDeAlumnos();
+                    break;
+                case NUMEROS:
+                    fabrica = new FabricaDeNumeros();
+                    break;
+                case VENDEDORES:
+                    fabrica = new FabricaDeVendedores();
+                    break;
+                case ALUMNOS_MUY_ESTUDIOSOS:
+                    fabrica = new FabricaDeAlumnosEstudiosos();
+                    break;
+                default:
+                    break;
+            }
+            return fabrica.crearPorTeclado();
+        }
+
+    }
+}
