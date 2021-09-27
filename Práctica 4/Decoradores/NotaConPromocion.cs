d5c6288eb02d9706231c644cae4a490d6b863056
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2021-09-27 01:47:57 -0300

    Práctica 4 - Se implementan las clases Auxiliares (AdapterAlumno, Decorator) y los distintos decoradores

---
+++ Práctica 4/Decoradores/NotaConPromocion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_4
+{
+    class NotaConPromocion : Decorator
+    {
+        public NotaConPromocion(IAlumno alum) : base(alum) { }
+        public override string mostrarCalificación()
+        {
+            int calificacion = alumno.getCalificación();
+            string muestroCalif = base.mostrarCalificación(), aux = null; 
+            int index = muestroCalif.LastIndexOf(")");
+
+            if (calificacion >= 7)
+                aux = "PROMOCIÓN";
+            if (calificacion >= 4 && calificacion < 7)
+                aux = "APROBADO";
+            if (calificacion < 4)
+                aux = "DESAPROBADO";
+
+            string nuevo = muestroCalif.Insert(index + 1, "(" + aux + ")");
+            return nuevo;    
+        }
+    }
+}
