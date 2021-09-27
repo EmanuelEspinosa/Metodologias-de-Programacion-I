140618b58ef91c66c1f31e9ee4190a24d860da95
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2021-09-27 01:24:14 -0300

    Práctica 4 - Se implementan las interfaces requeridas y las clases concideradas entidades del proyecto

---
+++ Práctica 4/Interfaces/FabricaDeComparables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_4
+{
+    interface FabricaDeComparables
+    {
+        Comparable crearAleatorio();
+        Comparable crearPorTeclado();
+    }
+}
