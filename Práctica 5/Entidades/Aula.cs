3c6a5797f9d3e0d51014407a3888dd11a97592db
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2021-10-03 20:03:36 -0300

    Práctica 5 - Se agrega la clase Aula y la interface OrdenEnAula1

---
+++ Práctica 5/Entidades/Aula.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_5
+{
+    class Aula
+    {
+        Teacher teacher;
+        IAlumno alumno;
+
+        public Aula(Teacher teacher)
+        {
+            this.teacher = teacher;
+        }
+        public void comenzar()
+        {
+            Console.WriteLine("Instanciando un Teacher");
+            teacher = new Teacher();
+        }
+        public void nuevoAlumno(IAlumno alum)
+        {
+            teacher.goToClass(new AdapterAlumno(alumno));
+        }
+        public void claseLista()
+        {
+            teacher.teachingAClass();
+        }
+    }
+}
---
+++ Práctica 5/Interfaces/OrdenEnAula1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Práctica_5
+{
+    interface OrdenEnAula1
+    {
+        void ejecutar();
+    }
+}
