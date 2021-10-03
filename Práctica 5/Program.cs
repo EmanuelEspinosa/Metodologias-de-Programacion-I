6a69931efc2b2ea5b0ff2a75ebf8a4cdec823bd3
Emanuel Espinosa <emanuel.espinosa.roberto@gmail.com>
2021-10-03 17:13:14 -0300

    Práctica 5 - Se implementa el patrón Proxy para Coleccionables. Se agrega la clase PilaProxy

--- Práctica 5/Program.cs
+++ Práctica 5/Program.cs
@@ -6,33 +6,47 @@ namespace Práctica_5
     {
         static void Main(string[] args)
         {
-            Teacher maestro = new Teacher();
-            GeneradorDeDatosAleatorios gen = new GeneradorDeDatosAleatorios();
+            //Teacher maestro = new Teacher();
+            //GeneradorDeDatosAleatorios gen = new GeneradorDeDatosAleatorios();
 
-            for (int i = 0; i < 20; i++)
-            {
-                if (i <= 10)
-                {
-                    IAlumno alum = new ProxyAlumno(gen.stringAleatorio(10), gen.numeroAleatorio(500), 1);
-                    IAlumno decorar = new NotaConLegajo(alum);
-                    decorar = new NotaConPromocion(decorar);
-                    decorar = new NotaConAsteriscos(decorar);
-                    decorar = new NotaEnLetras(decorar);
-                    decorar = new NotaSecuencial(decorar);
-                    maestro.goToClass(new AdapterAlumno(decorar));
-                }
-                if (i > 10)
-                {
-                    IAlumno alum = new ProxyAlumno(gen.stringAleatorio(10), gen.numeroAleatorio(500), 4);
-                    IAlumno decorar = new NotaConLegajo(alum);
-                    decorar = new NotaConPromocion(decorar);
-                    decorar = new NotaConAsteriscos(decorar);
-                    decorar = new NotaEnLetras(decorar);
-                    decorar = new NotaSecuencial(decorar);
-                    maestro.goToClass(new AdapterAlumno(decorar));
-                }
-            }
-            maestro.teachingAClass();
+            //for (int i = 0; i < 20; i++)
+            //{
+            //    if (i <= 10)
+            //    {
+            //        IAlumno alum = new ProxyAlumno(gen.stringAleatorio(10), gen.numeroAleatorio(500), 1);
+            //        IAlumno decorar = new NotaConLegajo(alum);
+            //        decorar = new NotaConPromocion(decorar);
+            //        decorar = new NotaConAsteriscos(decorar);
+            //        decorar = new NotaEnLetras(decorar);
+            //        decorar = new NotaSecuencial(decorar);
+            //        maestro.goToClass(new AdapterAlumno(decorar));
+            //    }
+            //    if (i > 10)
+            //    {
+            //        IAlumno alum = new ProxyAlumno(gen.stringAleatorio(10), gen.numeroAleatorio(500), 4);
+            //        IAlumno decorar = new NotaConLegajo(alum);
+            //        decorar = new NotaConPromocion(decorar);
+            //        decorar = new NotaConAsteriscos(decorar);
+            //        decorar = new NotaEnLetras(decorar);
+            //        decorar = new NotaSecuencial(decorar);
+            //        maestro.goToClass(new AdapterAlumno(decorar));
+            //    }
+            //}
+            //maestro.teachingAClass();
+
+            Coleccionable colec = new PilaProxy();
+            colec.agregar(new Numero(5));
+            colec.agregar(new Numero(10));
+            colec.agregar(new Numero(3));
+            Console.WriteLine("La colección tiene {0} elemetos",colec.Cuantos());
+            Console.WriteLine("El minimo elemento es: {0}",colec.minimo());
+            Console.WriteLine("El máximo elemento es: {0}",colec.maximo());
+
+            colec.agregar(new Numero(2));
+            Console.WriteLine("El minimo elemento es: {0}", colec.minimo());
+            //colec.agregar(new Numero(3));
+            //Console.WriteLine(colec.minimo());
+
             Console.ReadKey();
         }
     }
